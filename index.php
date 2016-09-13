9fa575557bc4bd1070f512a120d32cb1c17091d4
cyrille <cyrillemichard38@gmail.com>
2016-09-13 22:11:43 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<TITLE> <?php echo $titlePage ?> </TITLE>
+	<link rel="stylesheet" href="assets/CSS/styles.css">
+	
+</head>
+	<body>
+		<form method="post" >
+		
+		<label>Login: <input type="text" name="login"/></label><br/>
+		<label>Mot de passe: <input type="password" name="passe"/></label><br/>
+		<input type="submit" name="btValider" value="Valider"/>
+</form>
+	</body>
+</html>
